70363b62fba6a17f0dc490e334a989881589cffe
Jared Everett <jarsever@gmail.com>
2014-07-28 14:21:53 -0600

    blah blah blah.

--- lib/chef_metal/transport/ssh.rb
+++ lib/chef_metal/transport/ssh.rb
@@ -44,12 +44,14 @@ module ChefMetal
 
               channel.on_data do |ch2, data|
                 stdout << data
-                stream_chunk(execute_options, data, nil)
+                execute_options[:stream_stdout].print data
+                # stream_chunk(execute_options, data, nil)
               end
 
               channel.on_extended_data do |ch2, type, data|
                 stderr << data
-                stream_chunk(execute_options, nil, data)
+                execute_options[:stream_stderr].print data
+                # stream_chunk(execute_options, nil, data)
               end
 
               channel.on_request "exit-status" do |ch, data|
