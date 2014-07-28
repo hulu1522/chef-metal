d4544f499cf50296f2389f3bb6c0394a61d0e698
Jared Everett <jarsever@gmail.com>
2014-07-28 12:52:11 -0600

    puttin

--- lib/chef_metal/transport.rb
+++ lib/chef_metal/transport.rb
@@ -61,8 +61,9 @@ module ChefMetal
       else
         if stdout_chunk
           if options[:stream_stdout]
-            Chef::Log.info(stdout_chunk)
+            #Chef::Log.info(stdout_chunk)
             #puts stdout_chunk
+            stdout_chunk.puts
             #options[:stream_stdout].print stdout_chunk
           elsif options[:stream] || config[:log_level] == :debug
             STDOUT.print stdout_chunk
